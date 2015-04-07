9da0dafcbb60d42850aa03f8c2afb6f09a2ae46a
Daniel Ritter <daniel.ritter@designr.ch>
2015-04-07 10:28:44 +0200

    RestUri deleted

--- android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/service/RestApiInterface.java
+++ android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/service/RestApiInterface.java
@@ -1,46 +1,45 @@
 package ch.uzh.ifi.seal.soprafs15.group_09_android.service;
 
-import java.util.List;
-
 import ch.uzh.ifi.seal.soprafs15.group_09_android.models.Game;
-import ch.uzh.ifi.seal.soprafs15.group_09_android.models.RestUri;
 import ch.uzh.ifi.seal.soprafs15.group_09_android.models.User;
 import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
 
+import java.util.List;
+
 public interface RestApiInterface {
 
     /**
      * Returns all users
      *
-     * @param cb
+     * @param callback
      */
     @GET("/users")
-    void getUsers(Callback<List<User>> cb);
+    void getUsers(Callback<List<User>> callback);
 
     /**
      * Register a new user on the server
      * @User user: User to register
-     * @param cb
+     * @param callback
      */
     @POST("/users")
-    void createUser(@Body User user, Callback<RestUri> cb);
+    void createUser(@Body User user, Callback<User> callback);
 
     /**
      * Returns all games
-     * @param cb
+     * @param callback
      */
     @GET("/games")
-    void getGames(Callback<List<Game>> cb);
+    void getGames(Callback<List<Game>> callback);
 
     /**
      * Creates a new game
      * @Game game: The new game
-     * @param cb
+     * @param callback
      */
     @POST("/games")
-    void createGame(@Body Game game, Callback<RestUri> cb);
+    void createGame(@Body Game game, Callback<Game> callback);
 
 }
