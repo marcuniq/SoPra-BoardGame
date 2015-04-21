0545e644120adc67f42337781bf5f9ffde8bf731
Marco Unternährer <marco.unter@gmail.com>
2015-04-21 18:18:46 +0200

    exposing /backend/reset endpoint for deleting all entities; for integration testing

---
+++ service/src/main/java/ch/uzh/ifi/seal/soprafs15/service/UtilityService.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.seal.soprafs15.service;
+
+import ch.uzh.ifi.seal.soprafs15.controller.GenericService;
+
+/**
+ * @author Marco
+ */
+public abstract class UtilityService extends GenericService {
+
+    public abstract Boolean reset();
+}
