51f55c36c790bf77470e1e0a44b4686740996249
Hakuna <peter.parat@hotmail.com>
2015-04-24 12:15:47 +0200

    Added Fail methods for ITs

--- service/src/test/java/ch/uzh/ifi/seal/soprafs15/controller/GameServiceControllerIT.java
+++ service/src/test/java/ch/uzh/ifi/seal/soprafs15/controller/GameServiceControllerIT.java
@@ -84,44 +84,50 @@ public class GameServiceControllerIT {
         //TestUtils.clearRepositories(template, base);
     }
 
-//    @Test
-//    @SuppressWarnings("unchecked")
-//    public void testCreateGameFail() {
-//
-//        // Server Reset: Clean Repos
-//        //TestUtils.clearRepositories(template, base);
-//
-//        // Set up
-//
-//        List<GameResponseBean> gamesBefore = template.getForObject(base + "/games", List.class);
-//        Assert.assertEquals(0, gamesBefore.size());
-//
-//        UserRequestBean userRequest = TestUtils.toUserRequestBean(27, "TestUser");
-//
-//        TestUtils.createUser(userRequest, template, base);
-//
-//        ResponseEntity<UserLoginLogoutResponseBean> loginResponse = TestUtils.loginUser(1, template, base);
-//        String token = loginResponse.getBody().getToken();
-//
-//        GameRequestBean gameRequest = TestUtils.toGameRequestBean("TestGame1", token);
-//        ResponseEntity<GameCreateResponseBean> gameResponse = TestUtils.createGame(gameRequest, template, base);
-//
-//        List<GameResponseBean> gamesAfter = template.getForObject(base + "/games", List.class);
-//        Assert.assertEquals(1, gamesAfter.size());
-//
-//        UserRequestBean userRequest2 = TestUtils.toUserRequestBean(57, "Hanfred");
-//
-//        TestUtils.createUser(userRequest2, template, base);
-//
-//        ResponseEntity<UserLoginLogoutResponseBean> loginResponse2 = TestUtils.loginUser(1, template, base);
-//        String token2 = loginResponse2.getBody().getToken();
-//
-//        GameRequestBean gameRequest2 = TestUtils.toGameRequestBean("TestGame1", token);
-//        ResponseEntity<GameCreateResponseBean> gameResponse2 = TestUtils.createGame(gameRequest2, template, base);
-//
-//        // Server Reset: Clean Repos
-//        //TestUtils.clearRepositories(template, base);
-//    }
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testCreateGameFail() {
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+
+        // Set up
+
+        List<GameResponseBean> gamesBefore = template.getForObject(base + "/games", List.class);
+        Assert.assertEquals(0, gamesBefore.size());
+
+        UserRequestBean userRequest = TestUtils.toUserRequestBean(27, "TestUser");
+
+        TestUtils.createUser(userRequest, template, base);
+
+        ResponseEntity<UserLoginLogoutResponseBean> loginResponse = TestUtils.loginUser(1, template, base);
+        String token = loginResponse.getBody().getToken();
+
+        GameRequestBean gameRequest = TestUtils.toGameRequestBean("TestGame1", token);
+        ResponseEntity<GameCreateResponseBean> gameResponse = TestUtils.createGame(gameRequest, template, base);
+
+        List<GameResponseBean> gamesAfter = template.getForObject(base + "/games", List.class);
+        Assert.assertEquals(1, gamesAfter.size());
+
+        // After First Game is created, create a 2nd user that tries to create new Game with same name (Conflict)
+
+        UserRequestBean userRequest2 = TestUtils.toUserRequestBean(57, "Hanfred");
+
+        TestUtils.createUser(userRequest2, template, base);
+
+        ResponseEntity<UserLoginLogoutResponseBean> loginResponse2 = TestUtils.loginUser(1, template, base);
+        String token2 = loginResponse2.getBody().getToken();
+
+        GameRequestBean gameRequest2 = TestUtils.toGameRequestBean("TestGame1", token);
+        // TODO Exception GameAlreadyExistsResponseBean instead of GameCreateResponseBean
+        ResponseEntity<GameCreateResponseBean> gameResponse2 = TestUtils.createGame(gameRequest2, template, base);
+
+        List<GameResponseBean> gamesAfterSecond = template.getForObject(base + "/games", List.class);
+        Assert.assertEquals(1, gamesAfterSecond.size());
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+    }
 
     @Test
     @SuppressWarnings("unchecked")
@@ -163,6 +169,58 @@ public class GameServiceControllerIT {
 
         List<GamePlayerResponseBean> playersAfter = template.getForObject(base + "/games/1/players", List.class);
         Assert.assertEquals(2, playersAfter.size());
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testAddPlayerFail() {
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+
+        // Set up
+
+        UserRequestBean ownerRequest = TestUtils.toUserRequestBean(64, "TestOwner");
+
+        TestUtils.createUser(ownerRequest, template, base);
+
+        ResponseEntity<UserLoginLogoutResponseBean> ownerLoginResponse = TestUtils.loginUser(1, template, base);
+        String ownerToken = ownerLoginResponse.getBody().getToken();
+
+        GameRequestBean gameRequest = TestUtils.toGameRequestBean("TestGame1", ownerToken);
+        ResponseEntity<GameCreateResponseBean> gameResponse = TestUtils.createGame(gameRequest, template, base);
+
+        // Create new player and add it to the created game
+
+        List<GamePlayerResponseBean> playersBefore = template.getForObject(base + "/games/1/players", List.class);
+        Assert.assertEquals(1, playersBefore.size());
+
+        UserRequestBean userRequest = TestUtils.toUserRequestBean(33, "TestPlayer");
+        TestUtils.createUser(userRequest, template, base);
+
+        ResponseEntity<UserLoginLogoutResponseBean> loginResponse = TestUtils.loginUser(2, template, base);
+        String playerToken = loginResponse.getBody().getToken();
+
+        // Wrong game id: game does not exist
+
+        GamePlayerRequestBean playerRequest = TestUtils.toGamePlayerRequestBean(playerToken);
+        ResponseEntity<GameAddPlayerResponseBean> playerResponse = TestUtils.addPlayer(playerRequest, 2, template, base);
+
+        // Calculate Length of Channel Name
+        Long oracleLength = (long) "9b5eabcc-781b-483a-8eed-30d7eacb1567".length();
+
+        // TODO Exception Testing GameNotFoundException instead of GamePlayerResponseBean
+
+        // Calculate Length of Channel Name
+        //Long oracleLength = (long) "9b5eabcc-781b-483a-8eed-30d7eacb1567".length();
+
+        //Assert.assertEquals((long) oracleLength, playerResponse.getBody().getChannelName().length());
+
+        List<GamePlayerResponseBean> playersAfter = template.getForObject(base + "/games/1/players", List.class);
+        Assert.assertEquals(1, playersAfter.size());
 
         // Server Reset: Clean Repos
         //TestUtils.clearRepositories(template, base);
--- service/src/test/java/ch/uzh/ifi/seal/soprafs15/controller/UserServiceControllerIT.java
+++ service/src/test/java/ch/uzh/ifi/seal/soprafs15/controller/UserServiceControllerIT.java
@@ -74,6 +74,42 @@ public class UserServiceControllerIT {
 
     @Test
     @SuppressWarnings("unchecked")
+    public void testCreateUserFail() {
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+
+        List<UserResponseBean> usersBefore = template.getForObject(base + "/users", List.class);
+        Assert.assertEquals(0, usersBefore.size());
+
+        UserRequestBean request = TestUtils.toUserRequestBean(43, "TestUser1");
+
+        ResponseEntity<UserResponseBean> response = TestUtils.createUser(request, template, base);
+
+        Assert.assertEquals(request.getAge(), response.getBody().getAge());
+        Assert.assertEquals(request.getUsername(), response.getBody().getUsername());
+        Assert.assertEquals(1, (long) response.getBody().getId());
+
+        List<UserResponseBean> usersAfter = template.getForObject(base + "/users", List.class);
+        Assert.assertEquals(1, usersAfter.size());
+
+        // Create second user with same name as first user --> Fail
+
+        UserRequestBean secondUserRequest = TestUtils.toUserRequestBean(23, "TestUser1");
+
+        ResponseEntity<UserResponseBean> secondUserResponse = TestUtils.createUser(secondUserRequest, template, base);
+
+        // TODO UserAlreadyExistsException instead of UserResponseBean
+
+        List<UserResponseBean> usersAfterSecond = template.getForObject(base + "/users", List.class);
+        Assert.assertEquals(1, usersAfterSecond.size());
+
+        // Server Reset: Clean Repos
+        //TestUtils.clearRepositories(template, base);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
     public void testLoginUserSuccess() {
 
         // Server Reset: Clean Repos
