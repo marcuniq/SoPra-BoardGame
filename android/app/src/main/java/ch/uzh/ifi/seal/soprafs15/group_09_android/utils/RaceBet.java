9008798a5465594d330a953c1f203a63140145c8
Josua Fröhlich <kingjofroe@gmail.com>
2015-04-24 16:43:55 +0200

    popups extended

--- android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/utils/RaceBet.java
+++ android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/utils/RaceBet.java
@@ -4,43 +4,44 @@ import java.util.ArrayList;
 
 public class RaceBet {
     private ArrayList<Integer> raceBets = new ArrayList<>();
-    private ArrayList<Integer> raceBetButtons = new ArrayList<>();
-    private int raceBetPointer = 0;
+    private Integer characterCard;
+    private Integer characterCardButton;
 
-    public RaceBet() { }
-
-    public void add(ArrayList<Integer> someRaceBets) {
-        for (Integer raceBet : someRaceBets){
-            raceBets.add(raceBet);
-        }
+    public RaceBet(Integer characterCard, Integer characterCardButton) {
+        this.characterCard = characterCard;
+        this.characterCardButton = characterCardButton;
     }
 
-    public void add(Integer raceBet, Integer raceBetButton) {
+    public void add(Integer raceBet) {
         raceBets.add(raceBet);
-        raceBetButtons.add(raceBetButton);
     }
 
     public void clear(){
         raceBets.clear();
     }
 
-    public Integer getCurrentRaceBet() {
+    public ArrayList<Integer> getAllRaceBetCards() {
         if (raceBets.isEmpty()) return null;
-        else return raceBets.get(raceBetPointer);
-    }
-
-    public Integer getCurrentRaceBetButton() {
-        if (raceBetButtons.isEmpty()) return null;
-        else return raceBetButtons.get(raceBetPointer);
+        else return raceBets;
     }
 
     public boolean isEmpty(){
         return raceBets.isEmpty();
     }
 
-    public void setRaceBetPointer(int pos){
-        if (raceBets.isEmpty()) raceBetPointer = 0;
-        else if ( (pos > (raceBets.size() - 1)) || (pos < 0) ) raceBetPointer = (raceBets.size() - 1);
-        else raceBetPointer = pos;
+    public Integer getCharacterCard() {
+        return characterCard;
+    }
+
+    public Integer getCharacterCardButton() {
+        return characterCardButton;
+    }
+
+    public void setCharacterCard(Integer characterCard) {
+        this.characterCard = characterCard;
+    }
+
+    public void setCharacterCardButton(Integer characterCardButton) {
+        this.characterCardButton = characterCardButton;
     }
 }
