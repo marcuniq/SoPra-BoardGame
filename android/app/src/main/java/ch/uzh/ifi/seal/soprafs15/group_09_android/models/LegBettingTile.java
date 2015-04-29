e9bbfa640a4001072030d8534570826184215c52
Marco Unternährer <marco.unter@gmail.com>
2015-04-29 22:06:56 +0200

    changed LegBettingTile on Android to work with server; deleted unused Color enum

--- android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/models/LegBettingTile.java
+++ android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/models/LegBettingTile.java
@@ -7,21 +7,23 @@ import java.util.ArrayList;
 
 import auto.parcel.AutoParcel;
 import ch.uzh.ifi.seal.soprafs15.group_09_android.models.gson.AutoGson;
+import ch.uzh.ifi.seal.soprafs15.group_09_android.utils.GameColors;
 
 @AutoParcel @AutoGson
 public abstract class LegBettingTile implements Parcelable {
     @Nullable
     public abstract Long id();
     @Nullable
-    public abstract ArrayList<String> camel();
+    public abstract GameColors color();
     @Nullable
     public abstract Integer leadingPositionGain();
     @Nullable
     public abstract Integer secondPositionGain();
     @Nullable
     public abstract Integer otherPositionLoss();
-    public static LegBettingTile create(Long id, ArrayList<String> camel, Integer leadingPositionGain, Integer secondPositionGain, Integer otherPositionLoss) {
-        return new AutoParcel_LegBettingTile(id, camel, leadingPositionGain, secondPositionGain, otherPositionLoss);
+
+    public static LegBettingTile create(Long id, GameColors color, Integer leadingPositionGain, Integer secondPositionGain, Integer otherPositionLoss) {
+        return new AutoParcel_LegBettingTile(id, color, leadingPositionGain, secondPositionGain, otherPositionLoss);
     }
 }
 
--- android/app/src/main/java/ch/uzh/ifi/seal/soprafs15/group_09_android/models/beans/Color.java
+++
@@ -1,8 +0,0 @@
-package ch.uzh.ifi.seal.soprafs15.group_09_android.models.beans;
-
-/**
- * @author Marco
- */
-public enum Color {
-    WHITE, YELLOW, ORANGE, GREEN, BLUE
-}
