fbaa78c65bfd08925f769afbe4692d6b2e4aef63
Marco Unternährer <marco.unter@gmail.com>
2015-05-21 10:56:46 +0200

    deleting unnecessary repositories which are never accessed directly

--- service/src/main/java/ch/uzh/ifi/seal/soprafs15/model/repositories/LegBettingAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.seal.soprafs15.model.repositories;
-
-import ch.uzh.ifi.seal.soprafs15.model.game.LegBettingArea;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("legBettingAreaRepository")
-public interface LegBettingAreaRepository extends CrudRepository<LegBettingArea, Long> {
-}
--- service/src/main/java/ch/uzh/ifi/seal/soprafs15/model/repositories/RaceBettingAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.seal.soprafs15.model.repositories;
-
-import ch.uzh.ifi.seal.soprafs15.model.game.RaceBettingArea;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("raceBettingAreaRepository")
-public interface RaceBettingAreaRepository extends CrudRepository<RaceBettingArea, Long> {
-}
